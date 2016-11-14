7782c395267f2e336663ec80761b1bd2edc506d9
Bhavesh <bhaveshcomp@gmail.com>
2016-11-14 17:11:07 +0530

    Code update

---
+++ CategoriesDemo/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using CategoriesDemo.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CategoriesDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            try
+            {
+                var url = "http://www.igeniusgroup.com/people.json";
+                WebClient client = new WebClient();
+                string reply = client.DownloadString(url);
+                List<CustDetail> custDetail = JsonConvert.DeserializeObject<List<CustDetail>>(reply);
+                return View(custDetail);
+            }
+            catch(Exception ex)
+            {
+               throw ex;
+            }           
+        }
+    }
+}
---
+++ CategoriesDemo/Models/PetDetail.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CategoriesDemo.Models
+{
+
+    public class PetDetail
+    {
+        public string name { get; set; }
+        public string type { get; set; }
+    }
+
+    public class CustDetail
+    {
+        public string name { get; set; }
+        public string gender { get; set; }
+        public int age { get; set; }
+        public List<PetDetail> pets { get; set; }
+    }
+}
